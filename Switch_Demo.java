dee42a1b414adbce9bf94ea103b440e6a47a9db8
SubramaniMani <142685216+SubramaniMani@users.noreply.github.com>
2023-08-22 08:28:33 +0530

    switch_program

---
+++ Switch_Demo.java
@@ -0,0 +1,100 @@
+package Condional_operators;
+
+public class Switch_Demo {
+
+	public static void main(String[] args) {
+		
+		/*int Day=1;
+		switch(Day)
+		{	
+		case 1:
+			System.out.println("Sunday");
+			break;
+		case 2:
+			System.out.println("Monday");
+			break;
+		case 3:
+			System.out.println("Tuesday");
+			break;
+		case 4:
+			System.out.println("Wednesday");
+			break;
+		case 5:
+			System.out.println("Thursday");
+			break;
+		case 6:
+			System.out.println("Friday");
+			break;
+		case 7:
+			System.out.println("Saturday");
+			break;
+		default:
+			System.out.println("Invalid Day");	
+	}*/	
+		
+	 int  Blog = 4;
+	 switch(Blog)
+	 {
+	 case 1:
+	     System.out.println("Subramani");
+	     break;
+	 
+	 case 2:
+	     System.out.println("Sneka");
+	     break;
+	 
+	 case 3:
+         System.out.println("Ram_Kumar");
+         break;
+     
+	 case 4:
+		 System.out.println("Kabil_dev");
+		 break;
+		 
+	 case 5:
+		 System.out.println("Santhosh");
+		 break;
+		 
+	 case 6:
+		 System.out.println("kavi");
+		 break;
+		 
+	 case 7:
+		 System.out.println("Sheik");
+		 break;
+		 
+	 case 8:
+		 System.out.println("Petchi");
+		 break;
+		 
+	 default:
+		 System.out.println("Eight Members Only");
+	 
+		 
+	 }
+
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
+}
